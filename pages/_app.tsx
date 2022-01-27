268d15fdccdb6fc4bcde0e924a5722a1b3fcd864
Jose Antonio Albértuz <josealbertuz@gmail.com>
2022-01-27 12:32:47 +0100

    react query installed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,18 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../styles/global-styles'
 import theme from '../styles/theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 type NextPageWithLayout = NextPage & {
-  getLayout: (page: ReactElement) => ReactNode 
+  getLayout: (page: ReactElement) => ReactNode
 }
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
@@ -19,12 +23,15 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {
-        getLayout(<Component {...pageProps} />)
-      }
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <ReactQueryDevtools initialIsOpen={false} />
+        {
+          getLayout(<Component {...pageProps} />)
+        }
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
